248be075df7f654566f0b33353aea8153b7d3628
nemanjan00 <nemanjan00@gmail.com>
2018-12-21 18:23:01 +0100

    Complete documentation

--- src/routes/message/index.js
+++ src/routes/message/index.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
 
 /**
+ * @typedef Message
+ * @property {string} taskID.required
+ * @property {object} message.required - Payload to send to worker
+ */
+
+/**
  * This route is used for sending message to task runner. 
  * @route POST /message
+ * @consumes application/json
+ * @produces application/json
  * @group message - Message webhook
- * @param {string} email.query.required - username or email - eg: user@domain
- * @param {string} password.query.required - user's password.
- * @returns {object} 200 - An array of user info
+ * @param {Message.model} Message.body.required - Message to send to worker
+ * @returns {object} 200 - Execution status
  * @returns {Error}  default - Unexpected error
  */
 
--- src/routes/task/index.js
+++ src/routes/task/index.js
@@ -1,12 +1,21 @@
 const router = require("express").Router();
 
+// {"name":"sendSMS","task":{"to":"+423423423478","body":"Fdfsfd fwdsfwergf dfgdfg rgergerg."}}
+
+/**
+ * @typedef Task
+ * @property {string} name.required - Task name (name worker is using to listen on)
+ * @property {object} task.required - Task payload
+ */
+
 /**
  * This is used for creating new task on workerJS and establishing connection to listener. 
  * @route POST /task
+ * @consumes application/json
+ * @produces application/json
  * @group task - Task creation
- * @param {string} email.query.required - username or email - eg: user@domain
- * @param {string} password.query.required - user's password.
- * @returns {object} 200 - An array of user info
+ * @param {Task.model} Task.body.required - Params for Task
+ * @returns {object} 200 - Execution status
  * @returns {Error}  default - Unexpected error
  */
 
