64944ef91a3fbb012f33dad3d6fceea99dbf25ec
nemanjan00 <nemanjan00@gmail.com>
2018-12-21 17:03:16 +0100

    Document message route

--- src/routes/message/index.js
+++ src/routes/message/index.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 
+/**
+ * This function comment is parsed by doctrine
+ * @route POST /message
+ * @group message - Message webhook
+ * @param {string} email.query.required - username or email - eg: user@domain
+ * @param {string} password.query.required - user's password.
+ * @returns {object} 200 - An array of user info
+ * @returns {Error}  default - Unexpected error
+ */
+
 router.post("/", function (req, res) {
 	console.log(req.body);
 	res.json({
