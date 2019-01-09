ce28242c47b812dba043e3f1ea8e88feef2bb564
nemanjan00 <nemanjan00@gmail.com>
2019-01-09 01:25:16 +0100

    Remove limit

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json({limit: '200mb'})); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 module.exports = app;
